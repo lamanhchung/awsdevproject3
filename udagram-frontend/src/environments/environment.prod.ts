6747c4931155b4134c46eb691789a8b069885479
La Chung <manhchung.019@gmail.com>
2023-07-29 11:32:27 +0700

    Edit frontend endpoint to cloud

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad0536713aa6947bb860cccc1fa271b4-1642042119.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
